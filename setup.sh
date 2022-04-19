225d63b22cc194acb14e6f24ed32c8525f008d34
Alif Rachmawadi <arch@subosito.com>
2022-04-20 06:50:56 +0700

    add test mode

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 OS_NAME=$(echo "$RUNNER_OS" | awk '{print tolower($0)}')
 MANIFEST_BASE_URL="https://storage.googleapis.com/flutter_infra_release/releases"
 MANIFEST_URL="${MANIFEST_BASE_URL}/releases_${OS_NAME}.json"
+MANIFEST_TEST_PATH="test/releases_${OS_NAME}.json"
 
 # convert version like 2.5.x to 2.5
 normalize_version() {
@@ -18,14 +19,14 @@ latest_version() {
 }
 
 wildcard_version() {
-  if [ $2 == *"v"* ]; then  # is legacy version format
+  if [ $2 == *"v"* ]; then # is legacy version format
     if [[ $1 == any ]]; then
-    jq --arg version "$2" '.releases | map(select(.version | startswith($version) )) | first'
+      jq --arg version "$2" '.releases | map(select(.version | startswith($version) )) | first'
     else
-    jq --arg channel "$1" --arg version "$2" '.releases | map(select(.channel==$channel) | select(.version | startswith($version) )) | first'
+      jq --arg channel "$1" --arg version "$2" '.releases | map(select(.channel==$channel) | select(.version | startswith($version) )) | first'
     fi
   elif [[ $1 == any ]]; then
-    jq --arg version "$2" --arg arch "$ARCH" '.releases | map(select(.version | startswith($version)) | select(.dart_sdk_arch == null or .dart_sdk_arch == $arch)) | first'    
+    jq --arg version "$2" --arg arch "$ARCH" '.releases | map(select(.version | startswith($version)) | select(.dart_sdk_arch == null or .dart_sdk_arch == $arch)) | first'
   else
     jq --arg channel "$1" --arg version "$2" --arg arch "$ARCH" '.releases | map(select(.channel==$channel) | select(.version | startswith($version) ) | select(.dart_sdk_arch == null or .dart_sdk_arch == $arch)) | first'
   fi
@@ -39,13 +40,21 @@ get_version() {
   fi
 }
 
+get_release_manifest() {
+  if [ "$1" = true ]; then
+    cat $MANIFEST_TEST_PATH
+  else
+    curl --silent --connect-timeout 15 --retry 5 $MANIFEST_URL
+  fi
+}
+
 get_version_manifest() {
-  releases_manifest=$(curl --silent --connect-timeout 15 --retry 5 $MANIFEST_URL)
-  version_manifest=$(echo $releases_manifest | get_version $1 $(normalize_version $2))
+  release_manifest=$(get_release_manifest $1)
+  version_manifest=$(echo $release_manifest | get_version $2 $(normalize_version $3))
 
   if [[ $version_manifest == null ]]; then
     # fallback through legacy version format
-    echo $releases_manifest | wildcard_version $1 "v$(normalize_version $2)"
+    echo $releases_manifest | wildcard_version $2 "v$(normalize_version $3)"
   else
     echo $version_manifest
   fi
@@ -84,18 +93,38 @@ transform_path() {
   fi
 }
 
-CACHE_PATH=""
+help_usage() {
+  echo "USAGE:"
+  echo "$0 [-c cache/path] [-t] <channel> <version> <architecture>"
+}
 
-while getopts 'c:' flag; do
+CACHE_PATH=""
+TEST_MODE=false
+
+if [ "$#" -eq 0 ]; then
+  help_usage
+  exit 0
+fi
+
+while getopts ':h:dc:' flag; do
   case "${flag}" in
   c) CACHE_PATH="$OPTARG" ;;
-  ?) exit 2 ;;
+  d) TEST_MODE=true ;;
+  h)
+    help_usage
+    exit 0
+    ;;
   esac
 done
 
 CHANNEL="${@:$OPTIND:1}"
 VERSION="${@:$OPTIND+1:1}"
 ARCH="${@:$OPTIND+2:1}"
+
+if [[ -z $CHANNEL ]] || [[ -z $VERSION ]] || [[ -z $ARCH ]]; then
+  help_usage
+  exit 2
+fi
 
 SDK_CACHE="$(transform_path ${CACHE_PATH})"
 PUB_CACHE="$(transform_path ${CACHE_PATH}/.pub-cache)"
@@ -104,13 +133,24 @@ if [[ ! -x "${SDK_CACHE}/bin/flutter" ]]; then
   if [[ $CHANNEL == master ]]; then
     git clone -b master https://github.com/flutter/flutter.git "$SDK_CACHE"
   else
-    VERSION_MANIFEST=$(get_version_manifest $CHANNEL $VERSION)
+    VERSION_MANIFEST=$(get_version_manifest $TEST_MODE $CHANNEL $VERSION)
+
     if [[ $VERSION_MANIFEST == null ]]; then
       echo "Unable to determine Flutter version for channel: $CHANNEL version: $VERSION architecture: $ARCH"
       exit 1
-     fi
-    ARCHIVE_PATH=$(echo $VERSION_MANIFEST | jq -r '.archive')
-    download_archive "$ARCHIVE_PATH" "$SDK_CACHE"
+    fi
+
+    if [ "$TEST_MODE" = true ]; then
+      VERSION_DEBUG=$(echo $VERSION_MANIFEST | jq -j '.channel,":",.version,":",.dart_sdk_arch')
+
+      echo "$CHANNEL:$VERSION:$ARCH|$VERSION_DEBUG"
+      echo "---"
+      echo $VERSION_MANIFEST | jq
+      exit 0
+    else
+      ARCHIVE_PATH=$(echo $VERSION_MANIFEST | jq -r '.archive')
+      download_archive "$ARCHIVE_PATH" "$SDK_CACHE"
+    fi
   fi
 fi
 
